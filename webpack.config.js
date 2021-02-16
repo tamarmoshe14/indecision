d195d7906b9395ef7f1d9ef52184627408300715
Tamar Moshe <tamarmoshe14@gmail.com>
2021-02-16 14:23:29 +0200

    upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const isProduction = env === "production";
 
-module.exports = {
+module.exports = () =>  ({
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -16,9 +17,9 @@ module.exports = {
             use:['style-loader', 'css-loader', 'sass-loader']
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
 
-};
+});
