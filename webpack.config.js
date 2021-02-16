23d9ab92961027f6fa38e6c9e39e3f08162a66c9
Tamar Moshe <tamarmoshe14@gmail.com>
2021-02-16 14:31:15 +0200

    upload2

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
 const path = require('path');
-const isProduction = env === "production";
 
-module.exports = () =>  ({
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css$/,
-            use:['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
+process.env.NODE_ENV = process.env.NODE_ENV || "development"
 
-});
+module.exports = (env) =>  {
+    const isProduction = env === "production";
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{
+                test: /\.s?css$/,
+                use:['style-loader', 'css-loader', 'sass-loader']
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
+
+}};
